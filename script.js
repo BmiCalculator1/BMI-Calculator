bb17a82a1812588307ca52d486ac4310e3615cd9
BmiCalculator1 <162692696+BmiCalculator1@users.noreply.github.com>
2024-04-22 17:07:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('link1').addEventListener('click', function() {
+    document.getElementById('section').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('link2').addEventListener('click', function() {
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+});
